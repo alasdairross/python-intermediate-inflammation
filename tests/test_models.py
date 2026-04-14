82d672232cdd30ef9d5311f9a91c38619e7f5d82
Alasdair Ross <alasdair.ross@stfc.ac.uk>
2026-04-14 14:37:07 +0100

    add parametrised tests with pytest.mark.parameterize decorator

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,6 +4,7 @@ import numpy as np
 import numpy.testing as npt
 
 from inflammation.models import daily_mean, daily_max, daily_min
+import pytest
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -37,3 +38,39 @@ def test_daily_max():
     test_output = np.array([4,9,5])
 
     npt.assert_array_equal(daily_max(test_input), test_output)
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ]
+)
+def test_daily_mean(test,expected):
+    npt.assert_array_equal(daily_mean(np.array(test)),np.array(expected))
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [5, 6]),
+    ]
+)
+def test_daily_max(test,expected):
+    npt.assert_array_equal(daily_max(np.array(test)),np.array(expected))
+
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [1, 2]),
+    ]
+)
+def test_daily_min(test,expected):
+    npt.assert_array_equal(daily_min(np.array(test)),np.array(expected))
+
+
