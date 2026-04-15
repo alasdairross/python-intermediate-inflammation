dae938e14a3732d7d3d316f108504d1916699363
Alasdair Ross <alasdair.ross@stfc.ac.uk>
2026-04-15 12:38:00 +0100

    add normalise function with extra sanity checks, and type checking

--- inflammation/models.py
+++ inflammation/models.py
@@ -32,3 +32,23 @@ def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
 
+def patient_normalise(data):
+    """ Normalise patient data by the max for that patient (max of a given row)"""
+    # check data validity
+    if not isinstance(data, np.ndarray):
+        raise TypeError("Data must be numpy array")
+
+    if len(data.shape) != 2:
+            raise ValueError('inflammation array should be 2-dimensional')
+    
+    # dont allow negative data.
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
+
+    max = np.nanmax(data, axis = 1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data/ max[:,np.newaxis]
+
+    normalised[np.isnan(normalised)] = 0. #set nan values to zero
+    normalised[normalised < 0] = 0 # set negative normalised values to 0
+    return normalised
